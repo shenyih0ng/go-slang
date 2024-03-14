0d99855394ccc01b1d6a03ab2e15d20cd44b51d9
shenyihong <shen_yi_hong@outlook.com>
2024-03-14 22:21:33 +0800

    chore: format code

--- src/go-slang/lib/binaryOp.ts
+++ src/go-slang/lib/binaryOp.ts
@@ -1,96 +1,96 @@
-import { BinaryOperator } from "../types"
+import { BinaryOperator } from '../types'
 
 function evaluateArthmeticOp(operator: BinaryOperator, left: any, right: any): any {
-    let result = undefined
+  let result = undefined
 
-    switch (operator) {
-        case '+':
-          result = left + right
-          break
-        case '-':
-          result = left - right
-          break
-        case '*':
-          result = left * right
-          break
-        case '/':
-          // NOTE: this calculates the quotient
-          result = Math.floor(left / right)
-          break
-        case '%':
-          result = left % right
-          break
-    }
+  switch (operator) {
+    case '+':
+      result = left + right
+      break
+    case '-':
+      result = left - right
+      break
+    case '*':
+      result = left * right
+      break
+    case '/':
+      // NOTE: this calculates the quotient
+      result = Math.floor(left / right)
+      break
+    case '%':
+      result = left % right
+      break
+  }
 
-    return result
+  return result
 }
 
 function evaluateBitwiseOp(operator: BinaryOperator, left: any, right: any): any {
-    let result = undefined
+  let result = undefined
 
-    switch (operator) {
-        case '|':
-          result = left | right
-          break
-        case '^':
-          result = left ^ right
-          break
-    }
+  switch (operator) {
+    case '|':
+      result = left | right
+      break
+    case '^':
+      result = left ^ right
+      break
+  }
 
-    return result
+  return result
 }
 
 function evaluateRelationalOp(operator: BinaryOperator, left: any, right: any): any {
   let result = undefined
 
   switch (operator) {
-      case '==':
-        result = left === right
-        break
-      case '!=':
-        result = left !== right
-        break
-      case '<':
-        result = left < right
-        break
-      case '<=':
-        result = left <= right
-        break
-      case '>':
-        result = left > right
-        break
-      case '>=':
-        result = left >= right
-        break
+    case '==':
+      result = left === right
+      break
+    case '!=':
+      result = left !== right
+      break
+    case '<':
+      result = left < right
+      break
+    case '<=':
+      result = left <= right
+      break
+    case '>':
+      result = left > right
+      break
+    case '>=':
+      result = left >= right
+      break
   }
 
   return result
 }
 
-export function evaluateBinaryOp(operator: BinaryOperator,left: any, right: any): any {
-    let result = undefined
+export function evaluateBinaryOp(operator: BinaryOperator, left: any, right: any): any {
+  let result = undefined
 
-    switch (operator) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-          result = evaluateArthmeticOp(operator, left, right)
-          break
-        case '|':
-        case '^':
-          result = evaluateBitwiseOp(operator, left, right)
-          break
-        case '==':
-        case '!=':
-        case '<':
-        case '<=':
-        case '>':
-        case '>=':
-          result = evaluateRelationalOp(operator, left, right)
-          break
-    }
+  switch (operator) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+      result = evaluateArthmeticOp(operator, left, right)
+      break
+    case '|':
+    case '^':
+      result = evaluateBitwiseOp(operator, left, right)
+      break
+    case '==':
+    case '!=':
+    case '<':
+    case '<=':
+    case '>':
+    case '>=':
+      result = evaluateRelationalOp(operator, left, right)
+      break
+  }
 
-    return result
+  return result
 }
