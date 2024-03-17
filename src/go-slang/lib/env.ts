ba94ecc92b0dea958af131c095628b5e80d06a26
shenyihong <shen_yi_hong@outlook.com>
2024-03-17 14:49:01 +0800

    feat: implement abstraction for environment

---
+++ src/go-slang/lib/env.ts
@@ -0,0 +1,43 @@
+type Maybe<T> = T | null
+
+export class Environment {
+  private parent: Maybe<Environment> = null
+  private bindings: Map<string, any> = new Map()
+
+  constructor(bindings: { [key: string]: any }, parent: Maybe<Environment> = null) {
+    this.parent = parent
+    this.bindings = new Map(Object.entries(bindings))
+  }
+
+  public declare(name: string, value: any) {
+    this.bindings.set(name, value)
+  }
+
+  public assign(name: string, value: any): boolean {
+    if (!this.bindings.has(name)) {
+      return this.parent ? this.parent.assign(name, value) : false
+    }
+    this.bindings.set(name, value)
+    return true
+  }
+
+  public lookup(name: string): Maybe<any> {
+    if (!this.bindings.has(name)) {
+      return this.parent ? this.parent.lookup(name) : null
+    }
+    return this.bindings.get(name)
+  }
+
+  public extend(bindings: { [key: string]: any }): Environment {
+    return new Environment(bindings, this)
+  }
+}
+
+const predeclaredIdentifiers = {
+  true: true,
+  false: false
+}
+
+export function createGlobalEnvironment(): Environment {
+  return new Environment({ ...predeclaredIdentifiers })
+}
