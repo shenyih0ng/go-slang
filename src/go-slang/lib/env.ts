4d6b428394cb4abd29053438a0800b1dd7d00222
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-03-30 19:10:49 +0800

    refactor: introduce the construct of `Frame` to `Environment`
    This give us the ability to identify a frame with a numeric id instead
    of just using an object reference. As such, we will be able to serialize
    a reference to a environment in the heap

--- src/go-slang/lib/env.ts
+++ src/go-slang/lib/env.ts
@@ -2,41 +2,66 @@ import { PREDECLARED_IDENTIFIERS } from './predeclared'
 
 type Maybe<T> = T | null
 
+interface Frame {
+  id: number
+  bindings: Map<string, any>
+  parent: Maybe<Frame>
+}
 export class Environment {
-  private parent: Maybe<Environment> = null
-  private bindings: Map<string, any> = new Map()
+  private frameId = 0
+  private frameMap = new Map<number, Frame>()
 
-  constructor(bindings: { [key: string]: any }, parent: Maybe<Environment> = null) {
-    this.parent = parent
-    this.bindings = new Map(Object.entries(bindings))
+  private currFrame: Frame
+
+  constructor(bindings: { [key: string]: any }) {
+    this.currFrame = this.newFrame(bindings)
+  }
+
+  private newFrame(bindings: { [key: string]: any }, parent: Frame | null = null): Frame {
+    const frame = { id: this.frameId++, bindings: new Map(Object.entries(bindings)), parent }
+    this.frameMap.set(frame.id, frame)
+    return frame
+  }
+
+  public id(): number {
+    return this.currFrame.id
+  }
+
+  public setId(id: number): Environment {
+    this.currFrame = this.frameMap.get(id) as Frame
+    return this
   }
 
   public declare(name: string, value: any) {
-    this.bindings.set(name, value)
+    this.currFrame.bindings.set(name, value)
   }
 
   public declareZeroValue(name: string) {
-    // TEMP: assume all zero values are 0
-    this.bindings.set(name, 0)
+    this.currFrame.bindings.set(name, 0)
   }
 
   public assign(name: string, value: any): boolean {
-    if (!this.bindings.has(name)) {
-      return this.parent ? this.parent.assign(name, value) : false
+    let frame = this.currFrame
+    while (!frame.bindings.has(name)) {
+      if (!frame.parent) return false
+      frame = frame.parent
     }
-    this.bindings.set(name, value)
+    frame.bindings.set(name, value)
     return true
   }
 
   public lookup(name: string): Maybe<any> {
-    if (!this.bindings.has(name)) {
-      return this.parent ? this.parent.lookup(name) : null
+    let frame = this.currFrame
+    while (!frame.bindings.has(name)) {
+      if (!frame.parent) return null
+      frame = frame.parent
     }
-    return this.bindings.get(name)
+    return frame.bindings.get(name)
   }
 
   public extend(bindings: { [key: string]: any }): Environment {
-    return new Environment(bindings, this)
+    this.currFrame = this.newFrame(bindings, this.currFrame)
+    return this
   }
 }
 
