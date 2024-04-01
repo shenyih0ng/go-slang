f3a4b5ad58e089f3d2a2fc80f2fc6ea8a1b68445
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-04-01 19:16:53 +0800

    feat: randomize goroutine time quanta

--- src/go-slang/scheduler.ts
+++ src/go-slang/scheduler.ts
@@ -5,7 +5,8 @@ import { RuntimeSourceError } from '../errors/runtimeSourceError'
 type TimeQuanta = number
 
 export class Scheduler {
-  static TIME_QUANTA = 15
+  static MIN_TIME_QUANTA = 10
+  static MAX_TIME_QUANTA = 20
 
   private slangContext: SlangContext
   private routines: Array<[GoRoutine, TimeQuanta]> = []
@@ -14,8 +15,13 @@ export class Scheduler {
     this.slangContext = slangContext
   }
 
+  private static randTimeQuanta(): TimeQuanta {
+    const [minTQ, maxTQ] = [Scheduler.MIN_TIME_QUANTA, Scheduler.MAX_TIME_QUANTA]
+    return Math.floor(Math.random() * (maxTQ - minTQ) + minTQ)
+  }
+
   public schedule(routine: GoRoutine): void {
-    this.routines.push([routine, Scheduler.TIME_QUANTA])
+    this.routines.push([routine, Scheduler.randTimeQuanta()])
   }
 
   public run(): void {
@@ -40,7 +46,7 @@ export class Scheduler {
       if (routine.isMain && (routine.finished() || hasError)) { return } // prettier-ignore
 
       if (!routine.finished() && !hasError) {
-        this.routines.push([routine, Scheduler.TIME_QUANTA])
+        this.routines.push([routine, Scheduler.randTimeQuanta()])
       }
     }
   }
