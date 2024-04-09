4923321c2c0e7832301138df0484a09cec603871
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-04-09 11:40:32 +0800

    feat: implement buddy allocator in heap
    Standardize all heap address to be byte offset from the start address
    instead of word offset

--- src/go-slang/lib/heap/config.ts
+++ src/go-slang/lib/heap/config.ts
@@ -7,3 +7,6 @@ export const DEFAULT_HEAP_SIZE = 4096 // in words
 // The smallest addressable unit in the heap
 // We can think of it as the heap containing N number of words, each of size WORD_SIZE
 export const WORD_SIZE = 8 // in bytes
+
+// The byte offset to the size of a heap object within a tagged pointer
+export const SIZE_OFFSET = 5 // in bytes
--- src/go-slang/lib/heap/index.ts
+++ src/go-slang/lib/heap/index.ts
@@ -19,26 +19,73 @@ import {
 } from '../../types'
 import { AstMap } from '../astMap'
 import { BufferedChannel, UnbufferedChannel } from '../channel'
-import { DEFAULT_HEAP_SIZE, WORD_SIZE } from './config'
+import { DEFAULT_HEAP_SIZE, SIZE_OFFSET, WORD_SIZE } from './config'
 import { PointerTag } from './tags'
+
+function alignToWord(bytes: number) {
+  return Math.ceil(bytes / WORD_SIZE) * WORD_SIZE
+}
+
+function ceilPow2(n: number): number {
+  return 2 ** Math.ceil(Math.log2(n))
+}
+
+function buddyAddr(addr: number, bIdx: number): number {
+  return addr ^ (1 << bIdx)
+}
 
 export type HeapAddress = number
 
 export class Heap {
   private memory: DataView
-  private free: number = 0 // n_words to the next free block
+  // buddyBlocks[i] stores the set of free blocks of size 2^i
+  // free blocks are represented by their starting address
+  private buddyBlocks: Array<Set<number>>
 
   // we need to keep track of the AstMap to be able to resolve AST nodes stored in the heap
   private astMap: AstMap
 
   constructor(astMap: AstMap, n_words?: number) {
     this.astMap = astMap
-    this.memory = new DataView(new ArrayBuffer((n_words ?? DEFAULT_HEAP_SIZE) * WORD_SIZE))
 
-    // Allocate special values in the heap to avoid re-allocating them
-    // NOTE: these values are allocated at the beginning of the heap
-    this.allocateBoolean(false) // heap_addr: 0
-    this.allocateBoolean(true) // heap_addr: 1
+    const totalBytes = (n_words ?? DEFAULT_HEAP_SIZE) * WORD_SIZE
+    this.memory = new DataView(new ArrayBuffer(totalBytes))
+
+    const buddyAllocSize = Math.ceil(Math.log2(totalBytes))
+
+    this.buddyBlocks = Array.from({ length: buddyAllocSize + 1 }, () => new Set())
+    // initialize the block with the maximum size (= size of the entire heap)
+    this.buddyBlocks.at(-1)?.add(0)
+  }
+
+  private buddyAlloc(bytes: number): HeapAddress {
+    const allocSize = alignToWord(ceilPow2(bytes))
+    const bIdx = Math.log2(allocSize)
+
+    if (this.buddyBlocks[bIdx].size === 0 && !this.buddySplit(bIdx + 1)) {
+      return -1
+    }
+
+    const addr = this.buddyBlocks[bIdx].values().next().value
+    this.buddyBlocks[bIdx].delete(addr)
+    return addr
+  }
+
+  private buddySplit(idx: number): boolean {
+    if (idx >= this.buddyBlocks.length) {
+      return false
+    }
+
+    if (this.buddyBlocks[idx].size === 0 && !this.buddySplit(idx + 1)) {
+      return false
+    }
+
+    const addr = this.buddyBlocks[idx].values().next().value
+    this.buddyBlocks[idx].delete(addr)
+
+    this.buddyBlocks[idx - 1].add(addr)
+    this.buddyBlocks[idx - 1].add(buddyAddr(addr, idx - 1))
+    return true
   }
 
   /**
@@ -55,7 +102,7 @@ export class Heap {
     // JavaScript primitive values
     const valueType = typeof value
     if (valueType === 'boolean') {
-      return +value
+      return this.allocateBoolean(value)
     } else if (valueType === 'number') {
       return this.allocateNumber(value)
     }
@@ -118,67 +165,66 @@ export class Heap {
       return heap_addr
     }
 
-    const tag = this.tag(heap_addr)
-    const mem_addr = heap_addr * WORD_SIZE
+    const tag = this.memory.getInt8(heap_addr)
     switch (tag) {
       case PointerTag.False:
         return false
       case PointerTag.True:
         return true
       case PointerTag.Number:
-        return this.get(heap_addr + 1)
+        return this.memory.getFloat64(heap_addr + 1 * WORD_SIZE)
       case PointerTag.AstNode:
-        return this.astMap.get(this.memory.getInt16(mem_addr + 1))
+        return this.astMap.get(this.memory.getInt16(heap_addr + 1))
       case PointerTag.VarDeclOp:
         return {
           type: CommandType.VarDeclOp,
-          idNodeUid: this.memory.getInt16(mem_addr + 1),
-          zeroValue: this.memory.getInt8(mem_addr + 7) === 1
+          idNodeUid: this.memory.getInt16(heap_addr + 1),
+          zeroValue: this.memory.getInt8(heap_addr + 7) === 1
         } as VarDeclOp
       case PointerTag.AssignOp:
         return {
           type: CommandType.AssignOp,
-          idNodeUid: this.memory.getInt16(mem_addr + 1)
+          idNodeUid: this.memory.getInt16(heap_addr + 1)
         } as AssignOp
       case PointerTag.UnaryOp:
       case PointerTag.BinaryOp:
         return {
           type: tag === PointerTag.UnaryOp ? CommandType.UnaryOp : CommandType.BinaryOp,
-          opNodeId: this.memory.getInt16(mem_addr + 1)
+          opNodeId: this.memory.getInt16(heap_addr + 1)
         } as UnaryOp | BinaryOp
       case PointerTag.CallOp:
       case PointerTag.GoRoutineOp:
         return {
           type: tag === PointerTag.CallOp ? CommandType.CallOp : CommandType.GoRoutineOp,
-          calleeNodeId: this.memory.getInt16(mem_addr + 1),
-          arity: this.memory.getInt16(mem_addr + 3)
+          calleeNodeId: this.memory.getInt16(heap_addr + 1),
+          arity: this.memory.getInt16(heap_addr + 3)
         } as CallOp | GoRoutineOp
       case PointerTag.BuiltInOp:
         return {
           type: CommandType.BuiltinOp,
-          arity: this.memory.getInt16(mem_addr + 1),
-          id: this.memory.getInt16(mem_addr + 3)
+          arity: this.memory.getInt16(heap_addr + 1),
+          id: this.memory.getInt16(heap_addr + 3)
         } as BuiltinOp
       case PointerTag.ClosureOp:
         return {
           type: CommandType.ClosureOp,
-          funcDeclNodeUid: this.memory.getInt16(mem_addr + 1),
-          envId: this.memory.getInt16(mem_addr + 3)
+          funcDeclNodeUid: this.memory.getInt16(heap_addr + 1),
+          envId: this.memory.getInt16(heap_addr + 3)
         } as ClosureOp
       case PointerTag.EnvOp:
         return {
           type: CommandType.EnvOp,
-          envId: this.memory.getInt16(mem_addr + 1)
+          envId: this.memory.getInt16(heap_addr + 1)
         } as EnvOp
       case PointerTag.PopSOp:
         return PopS
       case PointerTag.UnbufferedChannel:
-        return new UnbufferedChannel(new DataView(this.memory.buffer, mem_addr, WORD_SIZE * 2))
+        return new UnbufferedChannel(new DataView(this.memory.buffer, heap_addr, WORD_SIZE * 2))
       case PointerTag.BufferedChannel:
         const chanMaxBufSize = this.size(heap_addr)
         const chanMemRegion = new DataView(
           this.memory.buffer,
-          mem_addr,
+          heap_addr,
           // +1 to include the tagged pointer
           WORD_SIZE * (chanMaxBufSize + 1)
         )
@@ -198,20 +244,14 @@ export class Heap {
   /* Memory Layout of a Number: [0-7:ptr][0-7:data] (2 words) */
   private allocateNumber(value: number): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.Number, 1)
-
-    const data_heap_addr = ptr_heap_addr + 1
-    this.set(data_heap_addr, value)
-
+    this.memory.setFloat64(ptr_heap_addr + WORD_SIZE, value)
     return ptr_heap_addr
   }
 
   /* Memory Layout of an AST Node: [0:tag, 1-2:astId, 3-4:_unused_, 5-6:size, 7:_unused_] (1 word) */
   private allocateAstNode({ uid }: Node): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.AstNode)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setInt16(ptr_mem_addr + 1, uid as number)
-
+    this.memory.setInt16(ptr_heap_addr + 1, uid as number)
     return ptr_heap_addr
   }
 
@@ -220,31 +260,22 @@ export class Heap {
     const ptr_heap_addr = this.allocateTaggedPtr(
       type === CommandType.UnaryOp ? PointerTag.UnaryOp : PointerTag.BinaryOp
     )
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setInt16(ptr_mem_addr + 1, opNodeId)
-
+    this.memory.setInt16(ptr_heap_addr + 1, opNodeId)
     return ptr_heap_addr
   }
 
   /* Memory Layout of a VarDeclOp: [0:tag, 1-2:idNodeUid, 3-4:_unused_, 5-6:size, 7:isZeroValue] (1 word) */
   private allocateVarDeclOp({ zeroValue, idNodeUid }: VarDeclOp): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.VarDeclOp)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setInt16(ptr_mem_addr + 1, idNodeUid)
-    this.memory.setInt8(ptr_mem_addr + 7, zeroValue ? 1 : 0)
-
+    this.memory.setInt16(ptr_heap_addr + 1, idNodeUid)
+    this.memory.setInt8(ptr_heap_addr + 7, zeroValue ? 1 : 0)
     return ptr_heap_addr
   }
 
   /* Memory Layout of an AssignOp: [0:tag, 1-2:idNodeUid, 3-4:_unused_, 5-6:size, 7:_unused_] (1 word) */
   private allocateAssignOp({ idNodeUid }: AssignOp): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.AssignOp)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setInt16(ptr_mem_addr + 1, idNodeUid)
-
+    this.memory.setInt16(ptr_heap_addr + 1, idNodeUid)
     return ptr_heap_addr
   }
 
@@ -253,50 +284,38 @@ export class Heap {
     const ptr_heap_addr = this.allocateTaggedPtr(
       type === CommandType.CallOp ? PointerTag.CallOp : PointerTag.GoRoutineOp
     )
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
     // NOTE: assume there will be no more than 2^16 AST nodes
-    this.memory.setInt16(ptr_mem_addr + 1, calleeNodeId)
+    this.memory.setInt16(ptr_heap_addr + 1, calleeNodeId)
     // NOTE: assume there will be no more than 2^16 arguments
-    this.memory.setInt16(ptr_mem_addr + 3, arity)
-
+    this.memory.setInt16(ptr_heap_addr + 3, arity)
     return ptr_heap_addr
   }
 
   /* Memory Layout of a BuiltinOp: [0:tag, 1-2: arity, 3-4:id, 5-6:size, 7:_unused_] (1 word) */
   private allocateBuiltinOp({ arity, id }: BuiltinOp): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.BuiltInOp)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
     // NOTE: assume there will be no arity greater than 2^16
-    this.memory.setInt16(ptr_mem_addr + 1, arity ?? -1)
+    this.memory.setInt16(ptr_heap_addr + 1, arity ?? -1)
     // NOTE: assume there are no more than 2^16 built-in operations
-    this.memory.setInt16(ptr_mem_addr + 3, id)
-
+    this.memory.setInt16(ptr_heap_addr + 3, id)
     return ptr_heap_addr
   }
 
   /* Memory Layout of a ClosureOp: [0:tag, 1-2:funcDeclNodeUid, 3-4:envId, 5-6:size, 7:_unused_] (1 word) */
   private allocateClosureOp({ funcDeclNodeUid, envId }: ClosureOp): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.ClosureOp)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
     // NOTE: assume there will be no more than 2^16 AST nodes
-    this.memory.setInt16(ptr_mem_addr + 1, funcDeclNodeUid)
+    this.memory.setInt16(ptr_heap_addr + 1, funcDeclNodeUid)
     // NOTE: assume there will be no more than 2^16 envs
-    this.memory.setInt16(ptr_mem_addr + 3, envId)
-
+    this.memory.setInt16(ptr_heap_addr + 3, envId)
     return ptr_heap_addr
   }
 
   /* Memory Layout of an EnvOp: [0:tag, 1-2:envId, 3-4:_unused_, 5-6:size, 7:_unused_] (1 word) */
   private allocateEnvOp({ envId }: EnvOp): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.EnvOp)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
     // NOTE: assume there will be no more than 2^16 envs
-    this.memory.setInt16(ptr_mem_addr + 1, envId)
-
+    this.memory.setInt16(ptr_heap_addr + 1, envId)
     return ptr_heap_addr
   }
 
@@ -305,12 +324,9 @@ export class Heap {
    */
   public allocateUnbufferedChan(): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.UnbufferedChannel, 2)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setInt16(ptr_mem_addr + 1, -1) // initialize recvId to -1
-    this.memory.setInt16(ptr_mem_addr + 3, -1) // initialize sendId to -1
-    this.memory.setUint8(ptr_mem_addr + 7, 0) // initialize hasSynced to false
-
+    this.memory.setInt16(ptr_heap_addr + 1, -1) // initialize recvId to -1
+    this.memory.setInt16(ptr_heap_addr + 3, -1) // initialize sendId to -1
+    this.memory.setUint8(ptr_heap_addr + 7, 0) // initialize hasSynced to false
     return ptr_heap_addr
   }
 
@@ -319,12 +335,9 @@ export class Heap {
    */
   public allocateBufferedChan(size: number): HeapAddress {
     const ptr_heap_addr = this.allocateTaggedPtr(PointerTag.BufferedChannel, size)
-
-    const ptr_mem_addr = ptr_heap_addr * WORD_SIZE
-    this.memory.setUint8(ptr_mem_addr + 1, 0) // initialize read index to 0
-    this.memory.setUint8(ptr_mem_addr + 2, 0) // initialize write index to 0
-    this.memory.setUint8(ptr_mem_addr + 3, 0) // initialize buffer size to 0
-
+    this.memory.setUint8(ptr_heap_addr + 1, 0) // initialize read index to 0
+    this.memory.setUint8(ptr_heap_addr + 2, 0) // initialize write index to 0
+    this.memory.setUint8(ptr_heap_addr + 3, 0) // initialize buffer size to 0
     return ptr_heap_addr
   }
 
@@ -335,34 +348,18 @@ export class Heap {
    * [0:tag, 1-4:_unused_, 5-6:size, 7:_unused_]
    *
    * @param tag  The tag to be associated with the pointer
-   * @param size The size of the underlying data structure
+   * @param size The size of the underlying data structure (in words)
    * @returns Address of the allocated block
    */
   private allocateTaggedPtr(tag: PointerTag, size: number = 0): HeapAddress {
-    const SIZE_OFFSET = 5 // in bytes
-
-    const alloc_heap_addr = this.free
-    // move the free pointer to the next block
-    // next free location: current free + size of the data + 1 (the pointer tag)
-    this.free += size + 1
-
-    const alloc_mem_addr = alloc_heap_addr * WORD_SIZE
+    // TODO handle out of memory error
+    const alloc_heap_addr = this.buddyAlloc((size + 1) * WORD_SIZE)
     // set the tag (1 byte) of the block
-    this.memory.setInt8(alloc_mem_addr, tag)
+    this.memory.setInt8(alloc_heap_addr, tag)
     // set the size (2 bytes) of the underlying data structure
-    this.memory.setUint16(alloc_mem_addr + SIZE_OFFSET, size)
+    this.memory.setUint16(alloc_heap_addr + SIZE_OFFSET, size)
 
     return alloc_heap_addr
-  }
-
-  /**
-   * Get the tag of the block at the given address
-   *
-   * @param heap_addr
-   * @returns tag of the block
-   */
-  private tag(heap_addr: HeapAddress): PointerTag {
-    return this.memory.getInt8(heap_addr * WORD_SIZE)
   }
 
   /**
@@ -372,26 +369,6 @@ export class Heap {
    * @returns size of the underlying data structure
    */
   private size(heap_addr: HeapAddress): number {
-    return this.memory.getUint16(heap_addr * WORD_SIZE + 5)
-  }
-
-  /**
-   * Get the raw word value at the given address
-   *
-   * @param heap_addr
-   * @returns raw word value at the given address
-   */
-  private get(heap_addr: HeapAddress): number {
-    return this.memory.getFloat64(heap_addr * WORD_SIZE)
-  }
-
-  /**
-   * Set word value at the given address
-   *
-   * @param heap_addr
-   * @param value value to be set
-   */
-  private set(heap_addr: number, value: number): void {
-    this.memory.setFloat64(heap_addr * WORD_SIZE, value)
+    return this.memory.getUint16(heap_addr + SIZE_OFFSET)
   }
 }
